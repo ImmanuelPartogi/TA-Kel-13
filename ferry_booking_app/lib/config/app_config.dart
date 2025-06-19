81e549a65bb2bbf987c6c2021babde6b49df3307
Immanuel Partogi Pardede <124246129+ImmanuelPartogi@users.noreply.github.com>
2025-06-19 14:22:37 +0700

    Ferry Booking Update

--- ferry_booking_app/lib/config/app_config.dart
+++ ferry_booking_app/lib/config/app_config.dart
@@ -2,19 +2,19 @@
 class AppConfig {
   // API Configuration
   // static const String apiBaseUrl = 'http://127.0.0.1:8000/api'; // Local development
-  static const String apiBaseUrl = 'http://192.168.40.145:8000/api';
+  static const String apiBaseUrl = 'http://172.27.65.163:8000/api';
 
   // Untuk akses dari perangkat lain atau Midtrans callback
-  static const String publicBaseUrl =
-      'https://cebd-114-5-144-192.ngrok-free.app';
+  // static const String publicBaseUrl =
+  //     'https://cebd-114-5-144-192.ngrok-free.app';
 
   // Midtrans Configuration
   static const String midtransClientKey = 'SB-Mid-client-8csuXJ7DmFhqmkMX';
   static const bool midtransProduction = false;
 
   // Callback URLs (harus URL publik yang bisa diakses Midtrans)
-  static const String midtransCallbackUrl =
-      'https://cebd-114-5-144-192.ngrok-free.app';
+  // static const String midtransCallbackUrl =
+  //     'https://cebd-114-5-144-192.ngrok-free.app';
 
   // Lainnya
   static const String appName = 'Ferry Booking';
